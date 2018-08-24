bd426470e2a1dae02e1758cd88f7c03004a2d191
Ada Lin <adalin@Adas-MacBook-Pro.local>
2018-08-24 18:45:25 -0400

    commit scraped reviews

--- src/scrape_reviews.py
+++ src/scrape_reviews.py
@@ -1,6 +1,7 @@
 import json
 import os
 import requests
+from bs4 import BeautifulSoup
 
 
 def get_phone_numbers(json_file):
@@ -36,13 +37,30 @@ def get_yelp_biz_data(phone_list):
     headers = {'Authorization': 'bearer %s' % os.environ['YELP_API_KEY']}
     biz_data = []
     for i, phone in enumerate(phone_list):
-        print(i)
+        print(i, ' of ', len(phone_list))
+
         params = {'phone': phone}
         r = requests.get('https://api.yelp.com/v3/businesses/search/phone',
                          params=params, headers=headers)
         biz_data.append(r.json())
 
     return biz_data
+
+
+def remove_all(lst, val):
+    '''Remove all instances of a given value from a list
+
+    Input
+    ------------
+    lst: list to iterate through
+    val: value to remove from list
+
+    Output
+    ------------
+    None
+
+    '''
+    lst[:] = (x for x in lst if x != val)
 
 
 def clean_biz_data(biz_data):
@@ -58,21 +76,87 @@ def clean_biz_data(biz_data):
     a cleaned list of metadata for relevant restaurants
 
     '''
-    for i, biz in enumerate(biz_data):
-        for j, listing in enumerate(biz['businesses']):
-            if listing['location']['state'] != 'NY':
-                print(listing['location']['state'])
-                del biz['businesses'][j]
-        if len(biz['businesses']) == 0:
-            del biz_data[i]
+    for biz in biz_data:
+        try:
+            for listing in biz['businesses']:
+                if listing['location']['state'] != 'NY':
+                    remove_all(biz['businesses'], listing)
+            if len(biz['businesses']) == 0:
+                remove_all(biz_data, biz)
+        except KeyError:
+            remove_all(biz_data, biz)
 
     return biz_data
 
 
 def get_urls(biz_data):
+    ''' Extract review page URLs from cleaned business metadata
+
+    Input
+    ------------
+    a list of metadata for restaurants (output of clean_biz_data)
+
+    Output
+    ------------
+    a list of unduplicated review page URLs
+
+    '''
+    review_urls = []
+    for biz in biz_data:
+        review_urls.append( [biz['businesses'][i]['url'] for i in range(len(biz['businesses'] )) ] )
+
+    # Flatten list and eliminate duplicate URLs:
+    review_urls = list(set([url for r in review_urls for url in r]))
+
+    # Clean URLs by removing UTM tags at the end:
+    sep = '?adjust_creative'
+    clean_urls = [r.split(sep)[0] for r in review_urls]
+
+    return clean_urls
 
 
+def get_reviews(review_urls):
+    ''' Scrape reviews from each page
 
+    Input
+    ------------
+    a list of URLs to review pages (output of get_urls)
+
+    Output
+    ------------
+    a list of dictionaries; each dictionary contains values for the keys 'phone'
+    and 'reviews'; 'reviews' value is a list of reviews scraped from the first
+    page of the restaurant's Yelp page
+
+    '''
+    yelp_reviews = []
+
+    for i, url in enumerate(review_urls):
+        print(i, ' of ', len(review_urls))
+
+        try:
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text, 'html.parser')
+
+            phone = soup.find(class_='biz-phone').text.strip().replace('(','+').replace(')','').replace('-','').replace(' ','')
+
+            reviews = []
+            for item in soup.find_all(class_='review-content'):
+                for p in item.find_all('p',{'lang':'en'}):
+                    for br in p.find_all('br'):
+                        br.replace_with('\n')
+                    reviews.append(p.text)
+
+            rest_dict = {'phone': phone}
+            rest_dict['reviews'] = reviews
+
+            yelp_reviews.append(rest_dict)
+
+        except AttributeError:
+            print('error at index ', i)
+            continue
+
+    return yelp_reviews
 
 
 if __name__ == '__main__':
@@ -88,3 +172,11 @@ if __name__ == '__main__':
     with open('data/cleaned_biz_data.json', 'w') as f:
         json.dump(biz_data, f)
     print('clean business data completed')
+
+    review_urls = get_urls(cleaned_data)
+    print('get review page URLs completed')
+
+    yelp_reviews = get_reviews(review_urls)
+    with open('data/reviews.json', 'w') as f:
+        json.dump(yelp_reviews, f)
+    print('get Yelp reviews completed...done scraping!')
